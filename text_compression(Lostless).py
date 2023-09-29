55018ea22a218d349fce0d9285f5d026af195d01
MatthewT95 <matt.taormina@outlook.com>
2023-09-29 16:59:17 -0400

    completed user input coding

--- text_compression(Lostless).py
+++ text_compression(Lostless).py
@@ -1,50 +1,97 @@
+import os
+
 uncompressed_text = "" # varible storing uncompressed text
 compressed_text = "" # varible storing compressed without string library
-substrings_and_frequencies = {} # varible storing list of substrings and frequencies in uncompressed text
-# varible storing string library
-# varible storing serialized string library
 compressed_data = "" # varible storing compressed_data
 source_filename = "" # varible storing source filename
-destination_filename = "" # varible storing destination filename
-# varible storing setting for verbose mode
-# varible storing compression mode (compress | decompress)
-# varible storing source_type mode (user_input | file)
+verbose_mode = False # varible storing setting for verbose mode
+compression_mode = "" # varible storing compression mode (compress | decompress)
+source_type = "" # varible storing source_type mode (user_input | file)
 file_content ="" # varible storing loaded file content
-max_substring_size = 8 # max size for substrings to search for
+partial_max_size = 8 # max size for substrings to search for
+lookback_window_max = 256
+valid_input = False # varible stores weither input was valid
+user_input = ""
+file_reader = ""
 
 # Ask user if they wish to compress or decompress
+valid_input = False
+while not valid_input:
+    user_input = input("Do you wish to compress(c) or decompress(d)")
+    if user_input == "c" or user_input == "C":
+        compression_mode = "compress"
+        valid_input = True
+    elif user_input == "d" or user_input == "D":
+        compression_mode = "decompress"
+        valid_input = True
+    else:
+        print("Invalid entry. Enter c to compress, d to decompress.")
+#print("compression_mode:" + compression_mode)
 # Ask user if they wish to input text or load a file
+valid_input = False
+while not valid_input:
+    user_input = input("Do you wish to use user_input(u) or file(f)")
+    if user_input == "u" or user_input == "U":
+        source_type = "user_input"
+        valid_input = True
+    elif user_input == "f" or user_input == "F":
+        source_type = "file"
+        valid_input = True
+    else:
+        print("Invalid entry. Enter u for user input, f for file.")
+#print("source_type:" + source_type)
+
 # ask user if they want verbose mode and set setting
+valid_input = False
+while not valid_input:
+    user_input = input("Do you wish to have verbose mode on y/n.")
+    if user_input == "y" or user_input == "Y":
+        verbose_mode = True
+        valid_input = True
+    elif user_input == "n" or user_input == "N":
+        verbose_mode = False
+        valid_input = True
+    else:
+        print("Invalid entry. Enter y for on, n for off.")
+#print("verbose_mode:" + str(verbose_mode))
 
 # if source_type is user_input
+if source_type == "user_input":
     # if compression_mode is compress
+    if compression_mode == "compress":
         # gets user input and stores it in uncompressed_text
+        uncompressed_text = input("Enter text to compress: ")
+        #print("compress: " + uncompressed_text)
     # if compression_mode is decompress
-        # gets user input and stores it in compressed_data
+    else:
+        # gets user input and stores it in compressed_text
+        compressed_text = input("Enter text to decompress: ")
+        #print("decompress: " + compressed_text)
 # if source_type is file
+else:
     # ask user for source file name and store in source_filename
-    # ask user for destination file name and store in destination_filename
+    source_filename = input("Please enter source filename: ")
     # check if file exists at source_filename
+    if os.path.isfile(source_filename):
+        #print("file exists.")
         # load file using source_filename and store content in file_contents
+        file_reader = open(source_filename, "r")
+        file_content = file_reader.read()
         # if compression_mode is compress
+        if compression_mode == "compress":
             # set uncompressed_text to file_contents
+            uncompressed_text = file_content
+            #print("compress: " + uncompressed_text)
         # if compression_mode is decompress
+        else:
             # set compressed_data to file_contents
+            compressed_text = file_content
+            #print("decompress: " + compressed_text)
     # if file dose not exist
+    else:
         # print error message
+        print("Source file dose not exist.")
         # exit program
+        exit()
 
 # if compression mode is compress
-    # get all substrings
-    # loop over all substring lenghts starting at max_substring_size
-        # loop over uncompressed_text starting from beginning to end - substring size
-            # stores current substring at given postion
-            # if substring is already in list
-                # increament frequency by 1
-            # if not present
-                # add to list with value of 1
-    
-    # if verbose mode is true
-        # print all substrings with frequencies
-    
-
