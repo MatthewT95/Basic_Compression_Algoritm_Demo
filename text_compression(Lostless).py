f9f77c7de684428c789a36082914535e5c2d811b
MatthewT95 <matt.taormina@outlook.com>
2023-09-29 15:43:13 -0400

    init text_compression_demo.py

---
+++ text_compression(Lostless).py
@@ -0,0 +1,10 @@
+# varible storing uncompressed text
+# varible storing compressed without string library
+# varible storing list of substrings and frequencies in uncompressed text
+# varible storing string library
+# varible storing serialized string library
+# varible storing compressed output
+# varible storing source filename
+# varible storing destination filename
+# varible storing setting for verbose mode
+
