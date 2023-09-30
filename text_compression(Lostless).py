42757c4c7a4e6146c6a24339d2720c34e6473044
MatthewT95 <matt.taormina@outlook.com>
2023-09-30 09:16:11 -0400

    compression optimized using unicode

--- text_compression(Lostless).py
+++ text_compression(Lostless).py
@@ -10,11 +10,13 @@ verbose_mode = False # varible storing setting for verbose mode
 compression_mode = "" # varible storing compression mode (compress | decompress)
 source_type = "" # varible storing source_type mode (user_input | file)
 file_content ="" # varible storing loaded file content
-partial_max_size = 64 # max size for substrings to search for
-lookback_window_max = 200 # look back window size, must be less then 255
+PARTIAL_MAX_SIZE = 7 # max size for substrings to search for. Must be less then 16
+BITS_FOR_SIZE_PORTION = 3
+lookback_window_max = 900 # look back window size, must be less then 255
 valid_input = False # varible stores weither input was valid
 user_input = ""
 file_reader = ""
+MAX_LOOKBACK_POINTER = 1000
 
 def string_to_numbers(input_string):
     output = ""
@@ -22,6 +24,21 @@ def string_to_numbers(input_string):
         output=output+str(ord(input_string[i])) + " "
 
     return (output)
+
+def compressed_string_breakdown(input_string):
+    output = ""
+    for i in range(0,len(input_string)):
+        size = ord(input_string[i]) % 2**BITS_FOR_SIZE_PORTION
+        pointer = (ord(input_string[i])-size) / 2**BITS_FOR_SIZE_PORTION
+        output=output+"({},{}) ".format(str(pointer),str(size))
+
+    return (output)
+def utf8len(s):
+    return len(s.encode('utf-8'))
+
+if PARTIAL_MAX_SIZE > 2**BITS_FOR_SIZE_PORTION:
+    print("PARTIAL_MAX_SIZE is too large for BIT_FOR_PORTION provided")
+    exit()
 
 # Ask user if they wish to compress or decompress
 valid_input = False
@@ -49,20 +66,6 @@ while not valid_input:
     else:
         print("Invalid entry. Enter u for user input, f for file.")
 #print("source_type:" + source_type)
-
-# ask user if they want verbose mode and set setting
-valid_input = False
-while not valid_input:
-    user_input = input("Do you wish to have verbose mode on y/n.")
-    if user_input == "y" or user_input == "Y":
-        verbose_mode = True
-        valid_input = True
-    elif user_input == "n" or user_input == "N":
-        verbose_mode = False
-        valid_input = True
-    else:
-        print("Invalid entry. Enter y for on, n for off.")
-#print("verbose_mode:" + str(verbose_mode))
 
 # if source_type is user_input
 if source_type == "user_input":
@@ -117,33 +120,43 @@ if compression_mode == "compress":
             lookback_window = uncompressed_text[0:postion]
         else:
             lookback_window = uncompressed_text[(postion-lookback_window_max):postion]
+        # reverses window so partial postions postions are relative to current postion
+        lookback_window=lookback_window[::-1]
         partial_found_in_lookback = False
         # loop over all substrings at postion with length starting at partial_max_size
-        for sustring_size in range(min(partial_max_size,len(uncompressed_text)-postion),0,-1):
+        for sustring_size in range(min(PARTIAL_MAX_SIZE,len(uncompressed_text)-postion),0,-1):
             # search for string in look back window
-            partial_postion_in_lookback_window = lookback_window.find(uncompressed_text[postion:postion+sustring_size])
+            partial_postion_in_lookback_window = lookback_window.find(uncompressed_text[postion+sustring_size:postion:-1])
             # if partial found
             if partial_postion_in_lookback_window != -1:
                 partial_found_in_lookback = True
                 # calculate look_back_pointer
-                lookback_pointer = len(lookback_window) - partial_postion_in_lookback_window
+                lookback_pointer = partial_postion_in_lookback_window + partial_size + 1
                 # update partial_size
                 partial_size = sustring_size
-                # append byte repressing lookbackpointer to compression_buffer
-                compression_buffer = compression_buffer + chr(lookback_pointer)
-                # append btye repressing partial_size to compression_buffer
-                compression_buffer = compression_buffer + chr(partial_size)
+                # combined pointer and size
+                serialized_pointer_size = chr((lookback_pointer * 2**BITS_FOR_SIZE_PORTION) + partial_size)
+                # # append byte repressing lookbackpointer to compression_buffer
+                # compression_buffer = compression_buffer + chr(lookback_pointer)
+                # # append btye repressing partial_size to compression_buffer
+                # compression_buffer = compression_buffer + chr(partial_size)
+                # append serialized_pointer_size to compressed_buffer
+                compression_buffer=compression_buffer+serialized_pointer_size
                 # incremeant postion by partial_size
                 postion=postion+partial_size
         # if partial not found
         if not partial_found_in_lookback:
-            # append byte representing zero pointer
-            compression_buffer = compression_buffer + chr(255)
-            # append charter at postion to compression_buffer
-            compression_buffer = compression_buffer + uncompressed_text[postion]
+            # combined pointer and size
+            serialized_pointer_size = chr(((MAX_LOOKBACK_POINTER+ord(uncompressed_text[postion])) * 2**BITS_FOR_SIZE_PORTION) + 0)
+            # # append byte representing zero pointer
+            # compression_buffer = compression_buffer + chr(255)
+            # # append charter at postion to compression_buffer
+            # compression_buffer = compression_buffer + uncompressed_text[postion]
             # incremeant postion by 1 to compression_buffer
+            # append serialized_pointer_size to compressed_buffer
+            compression_buffer=compression_buffer+serialized_pointer_size
             postion=postion+1
     # set compressed_text to compressed_buffer
     compressed_text = compression_buffer
-    print(string_to_numbers(compressed_text))
-    print(len(compressed_text),len(uncompressed_text))
+    print(compressed_string_breakdown(compressed_text))
+    print(utf8len(compressed_text),utf8len(uncompressed_text))
